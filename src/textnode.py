854dff2cab36d2dd3537c7aa4c1f4b340794e21f
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-04-26 19:47:28 +0100

    U: Annotated TextNode class with type hints and fixed bug in text_node_to_html_node function

--- src/textnode.py
+++ src/textnode.py
@@ -2,6 +2,17 @@ from enum import Enum
 from htmlnode import HTMLNode, LeafNode, ParentNode
 
 class TextType(Enum):
+    """
+    An enumeration representing different types of text elements.
+
+    Attributes:
+        TEXT (str): Represents plain text.
+        BOLD (str): Represents bold text.
+        ITALIC (str): Represents italicized text.
+        CODE (str): Represents a code snippet or inline code.
+        LINK (str): Represents a hyperlink.
+        IMAGE (str): Represents an image.
+    """
     TEXT = "text"
     BOLD = "bold"
     ITALIC = "italic"
@@ -10,33 +21,33 @@ class TextType(Enum):
     IMAGE = "image"
     
 class TextNode:
-    def __init__(self, text, text_type, url=None):
-        self.text = text
-        self.text_type = text_type
-        self.url = url
+    def __init__(self, text: str, text_type: TextType, url: str | None = None) -> None:
+        self.text: str = text
+        self.text_type: TextType = text_type
+        self.url: str | None = url
 
-    def __eq__(self: 'TextNode', other: object):
+    def __eq__(self, other: object) -> bool:
         if not isinstance(other, TextNode):
             return False
         return self.text == other.text and self.url == other.url and self.text_type == other.text_type
     
-    def __repr__(self: 'TextNode'):
+    def __repr__(self) -> str:
         return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
 
  
-def text_node_to_html_node(text_node:TextNode) -> HTMLNode:
+def text_node_to_html_node(text_node: TextNode) -> HTMLNode:
     match text_node.text_type:
         case TextType.TEXT:
-            return LeafNode(None, text_node.text)
+            return LeafNode(tag=None, value=text_node.text)
         case TextType.BOLD:
-            return LeafNode("b", text_node.text)
+            return LeafNode(tag="b", value=text_node.text)
         case TextType.ITALIC:
-            return LeafNode("i", text_node.text)
+            return LeafNode(tag="i", value=text_node.text)
         case TextType.CODE:
-            return LeafNode("code", value=text_node.text)
+            return LeafNode(tag="code", value=text_node.text)
         case TextType.LINK:
-            return LeafNode("a", text_node.text, {"href": text_node.url})
+            return LeafNode(tag="a", value=text_node.text, props={"href": text_node.url})
         case TextType.IMAGE:
-            return LeafNode("img", "", {"src": text_node.url, "alt": text_node.text})
+            return LeafNode(tag="img", value="", props={"src": text_node.url, "alt": text_node.text})
         case _:
             raise ValueError(f"Unknown text type: {text_node.text_type}")
