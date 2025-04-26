ac94eec950c5cfb085a6a5b577335d6849d1ae3f
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-04-26 19:36:28 +0100

    S: Restructured generate page and extract title function into thr gen_content module

--- src/test_block_markdown.py
+++ src/test_block_markdown.py
@@ -223,20 +223,6 @@ the **same** even with inline stuff
             "<div><pre><code>This is text that _should_ remain\nthe **same** even with inline stuff\n</code></pre></div>",
         )
 
-    def test_extract_title_1(self):
-        title_text = "# This is a title"
-        title = extract_title(title_text)
-        self.assertEqual(title, "This is a title")
-        
-    def test_extract_title_2(self):
-        title_text = "#This is a title"
-        with self.assertRaises(Exception):
-            title = extract_title(title_text)
-        
-    def test_extract_title_3(self):
-        title_text = "## This is a boy"
-        with self.assertRaises(Exception):
-            title = extract_title(title_text)
 
 if __name__ == "__main__":
     unittest.main()
---
+++ src/test_gen_content.py
@@ -0,0 +1,22 @@
+import unittest
+from block_markdown import extract_title
+
+class TestGenContent(unittest.TestCase):
+    def test_extract_title_1(self):
+        title_text = "# This is a title"
+        title = extract_title(title_text)
+        self.assertEqual(title, "This is a title")
+
+    def test_extract_title_2(self):
+        title_text = "#This is a title"
+        with self.assertRaises(Exception):
+            title = extract_title(title_text)
+
+    def test_extract_title_3(self):
+        title_text = "## This is a boy"
+        with self.assertRaises(Exception):
+            title = extract_title(title_text)
+
+
+if __name__ == "__main__":
+    unittest.main()
