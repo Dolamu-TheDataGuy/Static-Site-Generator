7f0fa6f12e8131f2e1ee89148f4b966e786c312b
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-04-27 17:47:54 +0100

    Z: Configured main.py function to use generate_pages_recursively function

--- src/main.py
+++ src/main.py
@@ -1,22 +1,22 @@
 import os
 import shutil
 from web_copy import copy_items
-from gen_content import generate_page
+from gen_content import generate_pages_recursively
 
-source_dir = "./static"
+source_dir = "./static/images"
 
-dest_dir = "./public"
+dest_dir = "./public/images"
 
-from_path = "./content/index.md"
+from_path = "./content"
 template_path = "./template.html"
-dest_path = "./public/index.html"
+dest_path = "./public"
 
 def main():
     print("Deleting public directory...")
     if os.path.exists(dest_dir):
         shutil.rmtree(dest_dir)
     copy_items(source_dir, dest_dir)
-    generate_page(from_path, template_path, dest_path)
+    generate_pages_recursively(from_path, template_path, dest_path)
 
 if __name__ == "__main__":
     main()
