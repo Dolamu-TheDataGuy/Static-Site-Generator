c459daa4c57a13965cb1962339c5028ab7bade31
Dolamu-TheDataGuy <oludaredolamu@gmail.com>
2025-04-27 19:17:49 +0100

    AB: Added implementation to suit GitHub page hosting and buildfile in docs

--- src/gen_content.py
+++ src/gen_content.py
@@ -12,7 +12,7 @@ def extract_title(markdown):
     raise Exception("No title found in the markdown")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, base_path):
     print(f"Generating page from {from_path} to {dest_path} using {template_path}")
     with open(from_path, "r") as f:
         md_content = f.read()
@@ -23,7 +23,8 @@ def generate_page(from_path, template_path, dest_path):
     title = extract_title(md_content)
     template_content = template_content.replace("{{ Title }}", title)
     template_content = template_content.replace("{{ Content }}", md_html_content)
-
+    template_content = template_content.replace('href="/', f'href="{base_path}')
+    template_content = template_content.replace('src="/', f'src="{base_path}')
     if not os.path.exists(dest_path):
         os.makedirs(os.path.dirname(dest_path), exist_ok=True)
 
@@ -31,7 +32,7 @@ def generate_page(from_path, template_path, dest_path):
         f.write(template_content)
 
 
-def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursively(dir_path_content, template_path, dest_dir_path, base_path):
     folder_content = os.listdir(dir_path_content)
     
     for item in folder_content:
@@ -42,12 +43,11 @@ def generate_pages_recursively(dir_path_content, template_path, dest_dir_path):
             print(f"Found a directory: {item}")
             new_dest_dir_path = os.path.join(dest_dir_path, item)
             os.makedirs(new_dest_dir_path, exist_ok=True)
-            generate_pages_recursively(item_path, template_path, new_dest_dir_path)
+            generate_pages_recursively(item_path, template_path, new_dest_dir_path, base_path)
         
         # Base case: if item is a file or markdown file    
         elif item.endswith(".md"):
             print(f"Found a markdown file: Generating page for {item}")
             new_dest_path = os.path.join(dest_dir_path, item.replace(".md", ".html"))
-            generate_page(item_path, template_path, new_dest_path)
+            generate_page(item_path, template_path, new_dest_path, base_path)
             print(f"Generated page: {new_dest_path}")
-
--- src/main.py
+++ src/main.py
@@ -1,22 +1,28 @@
 import os
+import sys
 import shutil
 from web_copy import copy_items
 from gen_content import generate_pages_recursively
 
 source_dir = "./static/images"
 
-dest_dir = "./public/images"
+dest_dir = "./docs/images"
 
 from_path = "./content"
 template_path = "./template.html"
-dest_path = "./public"
+dest_path = "./docs"
+
+base_path = "/"
+
+if len(sys.argv) > 1:
+    base_path = sys.argv[1]
 
 def main():
     print("Deleting public directory...")
     if os.path.exists(dest_dir):
         shutil.rmtree(dest_dir)
     copy_items(source_dir, dest_dir)
-    generate_pages_recursively(from_path, template_path, dest_path)
+    generate_pages_recursively(from_path, template_path, dest_path, base_path)
 
 if __name__ == "__main__":
     main()
